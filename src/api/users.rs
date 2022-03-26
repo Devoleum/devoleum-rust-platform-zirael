652412871968df493acfa72c72e3ede5610afa37
Lorenzo Zaccagnini <lorenzozcg@gmail.com>
2022-03-26 16:03:22 +0100

    Update users.rs

--- src/api/users.rs
+++ src/api/users.rs
@@ -1,27 +1,29 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer};
-use mongodb::{bson::doc, Client, Collection,  error::Error, bson::Document};
+use crate::config::{Config, IConfig};
 use crate::models::response::{LoginResponse, Response};
 use crate::models::users::{Claims, Login, Register, User};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer};
 use chrono::{DateTime, Duration, Utc};
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use mongodb::{bson::doc, bson::Document, error::Error, Client, Collection};
 
 const DB_NAME: &str = "devoleumdb";
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("api/users")
-        .service(get_merchant)
-        .service(register_controller)
+    cfg.service(
+        web::scope("api/users")
+            .service(get_merchant)
+            .service(register_controller),
     );
 }
 
-async fn find_user_with_email(client: web::Data<Client>, email: String) -> Result<Option<User>, Error> {
+async fn find_user_with_email(
+    client: web::Data<Client>,
+    email: String,
+) -> Result<Option<User>, Error> {
     let collection: Collection<Document> = client.database(DB_NAME).collection("users");
-    let cursor =
-    collection
-        .find_one(doc! {"email": email}, None)
-        .await;
+    let cursor = collection.find_one(doc! {"email": email}, None).await;
     match cursor {
         Ok(Some(doc)) => match bson::from_bson(bson::Bson::Document(doc)) {
             Ok(model) => Ok(model),
@@ -53,22 +55,67 @@ async fn get_merchant(client: web::Data<Client>, id: web::Path<String>) -> HttpR
 async fn register_controller(user: web::Json<Register>, client: web::Data<Client>) -> HttpResponse {
     let user = user.into_inner();
     let collection: Collection<Document> = client.database(DB_NAME).collection("users");
-    let _exist = find_user_with_email(client, (&user.email).parse().unwrap())
-        .await;
+    let _exist = find_user_with_email(client, (&user.email).parse().unwrap()).await;
     match _exist {
-        Ok(Some(_)) => {
-            HttpResponse::Ok().json("This e-mail is using by some user, please enter another e-mail.")
-        }
+        Ok(Some(_)) => HttpResponse::Ok()
+            .json("This e-mail is using by some user, please enter another e-mail."),
         Ok(None) => {
             let mut sha = Sha256::new();
             sha.input_str(user.password.as_str());
             let hash_pw = sha.result_str();
             let _ex = collection.insert_one(doc! {"name": user.name, "email": user.email, "password": hash_pw, "isAdmin:": false}, None);
             match _ex.await {
-                Ok(_) =>HttpResponse::Ok().json("success"),
+                Ok(_) => HttpResponse::Ok().json("success"),
                 Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
             }
         }
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
+
+#[post("/login")]
+async fn login(user: web::Json<Login>, client: web::Data<Client>) -> HttpResponse {
+    match find_user_with_email(client, user.email.to_string()).await {
+        Ok(Some(x)) => {
+            let mut sha = Sha256::new();
+            sha.input_str(user.password.as_str());
+            if x.password == sha.result_str() {
+                // JWT
+                let _config: Config = Config {};
+                let _var = _config.get_config_with_key("SECRET_KEY");
+                let key = _var.as_bytes();
+
+                let mut _date: DateTime<Utc>;
+                // Remember Me
+                if !user.remember_me {
+                    _date = Utc::now() + Duration::hours(1);
+                } else {
+                    _date = Utc::now() + Duration::days(365);
+                }
+                let my_claims = Claims {
+                    sub: user.email,
+                    exp: _date.timestamp() as usize,
+                };
+                let token = encode(
+                    &Header::default(),
+                    &my_claims,
+                    &EncodingKey::from_secret(key),
+                )
+                .unwrap();
+                let obj = LoginResponse {
+                    status: true,
+                    token,
+                    message: "You have successfully logged in.".to_string(),
+                };
+                HttpResponse::Ok().json(obj)
+            } else {
+ 
+                Err(Response {
+                    status: false,
+                    message: "Check your user informations.".to_string(),
+                })
+            }
+        }
+        Ok(None) => HttpResponse::InternalServerError().body("not found"),
     }
 }
