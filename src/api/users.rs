fedee515a49f8b591a83cf68e3866e4d108d18c5
Lorenzo Zaccagnini <lorenzozcg@gmail.com>
2022-03-26 16:45:50 +0100

    blake2b

--- src/api/users.rs
+++ src/api/users.rs
@@ -3,10 +3,10 @@ use crate::models::response::{LoginResponse, Response};
 use crate::models::users::{Claims, Login, Register, User};
 use actix_web::{get, post, web, App, HttpResponse, HttpServer};
 use chrono::{DateTime, Duration, Utc};
-use crypto::digest::Digest;
-use crypto::sha2::Sha256;
+use blake2::{Blake2b512, Digest};
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use mongodb::{bson::doc, bson::Document, error::Error, Client, Collection};
+use hex_literal::hex;
 
 const DB_NAME: &str = "devoleumdb";
 
@@ -60,9 +60,9 @@ async fn register_controller(user: web::Json<Register>, client: web::Data<Client
         Ok(Some(_)) => HttpResponse::Ok()
             .json("This e-mail is using by some user, please enter another e-mail."),
         Ok(None) => {
-            let mut sha = Sha256::new();
-            sha.input_str(user.password.as_str());
-            let hash_pw = sha.result_str();
+            let mut hasher = Blake2b512::new();
+            hasher.update(user.password.as_str());
+            let hash_pw: String = format!("{:x}", hasher.finalize());
             let _ex = collection.insert_one(doc! {"name": user.name, "email": user.email, "password": hash_pw, "isAdmin:": false}, None);
             match _ex.await {
                 Ok(_) => HttpResponse::Ok().json("success"),
@@ -77,9 +77,10 @@ async fn register_controller(user: web::Json<Register>, client: web::Data<Client
 async fn login(user: web::Json<Login>, client: web::Data<Client>) -> HttpResponse {
     match find_user_with_email(client, user.email.to_string()).await {
         Ok(Some(x)) => {
-            let mut sha = Sha256::new();
-            sha.input_str(user.password.as_str());
-            if x.password == sha.result_str() {
+            let mut hasher = Blake2b512::new();
+            hasher.update(user.password.as_str());
+            let hash_pw: String = format!("{:x}", hasher.finalize());
+            if x.password == hash_pw {
                 // JWT
                 let _config: Config = Config {};
                 let _var = _config.get_config_with_key("SECRET_KEY");
@@ -109,11 +110,7 @@ async fn login(user: web::Json<Login>, client: web::Data<Client>) -> HttpRespons
                 };
                 HttpResponse::Ok().json(obj)
             } else {
- 
-                Err(Response {
-                    status: false,
-                    message: "Check your user informations.".to_string(),
-                })
+                HttpResponse::InternalServerError().body("error")
             }
         }
         Ok(None) => HttpResponse::InternalServerError().body("not found"),
