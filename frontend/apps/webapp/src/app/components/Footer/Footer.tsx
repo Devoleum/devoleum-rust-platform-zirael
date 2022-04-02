59729c3f95180c245baf24f6227ac3657097c10d
Lorenzo Zaccagnini <lorenzozcg@gmail.com>
2022-04-02 23:11:33 +0200

    no bs

--- frontend/apps/webapp/src/app/components/Footer/Footer.tsx
+++ frontend/apps/webapp/src/app/components/Footer/Footer.tsx
@@ -4,27 +4,35 @@ import { Container, Row, Col } from 'react-bootstrap';
 const Footer = () => {
   return (
     <footer>
-      <Container
-        style={{ fontSize: '0.6em', wordBreak: 'break-all', maxWidth: '90%' }}
+      <div
+        className="container text-space"
+        style={{
+          fontSize: '0.6em',
+          wordBreak: 'break-all',
+          maxWidth: '90%',
+          textAlign: 'center',
+          paddingBottom: '1rem !important',
+          paddingTop: '1rem !important',
+        }}
       >
         <div className="row">
-          <div className="col text-center  py-3">Copyright &copy; Devoleum</div>
+          <div className="col">Copyright &copy; Devoleum</div>
         </div>
         <div className="row">
-          <div className="col text-center">>Donate to</div>
+          <div className="col">Donate to</div>
         </div>
         <div className="row">
-          <div className="col text-center">>
+          <div className="col">
             ETH: 0xbf8d0d4be61De94EFCCEffbe5D414f911F11cBF8
           </div>
         </div>
         <div className="row">
-          <div className="col text-center">>
+          <div className="col">
             ALGO: 5N22O3PIXAGNAGHBFSU6HQ22KGI4D3XEBACEFODVH3UOKCA4C2IBRD4ZDE
           </div>
         </div>
         <br />
-      </Container>
+      </div>
     </footer>
   );
 };
--- frontend/apps/webapp/src/app/components/Header/Header.tsx
+++ frontend/apps/webapp/src/app/components/Header/Header.tsx
@@ -1,25 +1,15 @@
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <div>
-      <Navbar style={{ background: '#92AF31', height: '60px' }}>
-        <Container>
-          <Link to="/">
-            <Navbar.Brand
-              style={{
-                color: 'white',
-                position: 'absolute',
-                top: '12px',
-                left: '80px',
-              }}
-            >
-              Devoleum
-            </Navbar.Brand>
-          </Link>
-        </Container>
-      </Navbar>
+      <div
+        style={{ background: '#92AF31', height: '60px', textAlign: 'center' }}
+      >
+        <Link to="/">
+          <div className="navbar-brand">Devoleum</div>
+        </Link>
+      </div>
     </div>
   );
 };
